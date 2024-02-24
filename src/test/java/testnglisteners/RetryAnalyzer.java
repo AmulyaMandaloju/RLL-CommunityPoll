a93590ab1436519c90050398529c4f1abf1f7f04
Admin <admin@gmail.com>
2024-02-24 10:50:19 +0530

    added CommiunityPoll for RLL

---
+++ src/test/java/testnglisteners/RetryAnalyzer.java
@@ -0,0 +1,21 @@
+package testnglisteners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer{
+	int count=0;
+	int retrylimit=2;
+	
+
+	public boolean retry(ITestResult result) {
+		if(count<retrylimit) {
+			System.out.println("Retrying test: " + result.getName() + " for the " + (count + 1) + " time.");
+            count++;
+			return true;
+		}
+		
+		return false;
+	}
+
+}
